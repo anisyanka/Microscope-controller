8d7b3b42094bc4850dd1c293cd46161b58c34e6e
Aleksandr Anisimov <anisimov.alexander.s@gmail.com>
2024-02-10 07:45:23 +0300

    [web][front] Proper way to handle focus buttons click events for mobile and PC devices

--- web_viewer/static/scripts/focus_control.js
+++ web_viewer/static/scripts/focus_control.js
@@ -1,22 +1,44 @@
-var interval_id = 0
+var interval_id = 0;
+var timeout_id = 0;
+var was_btn_released = 0;
+
+function focus_control_pressed(sign) {
+    was_btn_released = 0;
+
+    if (timeout_id == 0) {
+        timeout_id = setTimeout(function() {focus_control_check_was_release(sign);}, 400);
+    }
+}
+
+function focus_control_released(sign) {
+    was_btn_released = 1;
+    focus_control_stop();
+
+    if (timeout_id) {
+        clearTimeout(timeout_id);
+        timeout_id = 0;
+    }
+}
+
+function focus_control_check_was_release(sign) {
+    if (was_btn_released == 0) { /* the user is still pressing the button */
+        focus_control_change_repeatedly(sign);
+    }
+    timeout_id = 0;
+}
 
 function focus_control(sign) {
-    var request = new XMLHttpRequest()
-
-    if (interval_id != 0) {
-        clearInterval(interval_id)
-        interval_id = window.setInterval(function() {focus_control(sign); }, 100);
-    }
+    var request = new XMLHttpRequest();
 
     request.onload = function() {
         if (request.response == "OK") {
             if (sign == "upper") {
-                console.log("Focus: + OK")
+                console.log("Focus: + OK");
             } else if (sign == "lower") {
-                console.log("Focus: - OK")
+                console.log("Focus: - OK");
             }
         } else {
-            console.log("Focus: Unable to change focus --> HTTP request error")
+            console.log("Focus: Unable to change focus --> HTTP request error");
         }
     }
 
@@ -27,12 +49,18 @@ function focus_control(sign) {
 }
 
 function focus_control_change_repeatedly(sign) {
-    interval_id = window.setInterval(function() {focus_control(sign); }, 10);
-    console.log("Change focus repeadatly started")
+    if (interval_id == 0) {
+        interval_id = window.setInterval(function() {focus_control(sign);}, 100);
+        console.log("Change focus repeadatly started");
+    }
 }
 
 function focus_control_stop() {
-    clearInterval(interval_id)
-    interval_id = 0
-    console.log("Change focus repeadatly stoped")
+    was_btn_released = 1;
+
+    if (interval_id) {
+        clearInterval(interval_id);
+        interval_id = 0;
+        console.log("Change focus repeadatly stoped");
+    }
 }
