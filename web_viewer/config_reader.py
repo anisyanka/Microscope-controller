79ac9c5ce94e154d294424bcdbe3f1627e2eef49
Aleksandr Anisimov <anisimov.alexander.s@gmail.com>
2024-02-11 15:35:57 +0300

    Implement modbus stubs

--- web_viewer/config_reader.py
+++ web_viewer/config_reader.py
@@ -8,10 +8,6 @@ def config_reader_retrieve_all_data():
         global mb_conv_data
         mb_conv_data = json.loads(j.read())
 
-    with open(microscope_cmd_conf, 'r') as m:
-        global microscope_data
-        microscope_data = json.loads(m.read())
-
 def config_reader_get_modbus_tcp_rtu_converter_port():
     return int(mb_conv_data['modbus_port'])
 
@@ -20,3 +16,38 @@ def config_reader_get_modbus_slave_id():
 
 def config_reader_get_modbus_slave_timeout():
     return int(mb_conv_data['modbus_loss_connection_timeout_ms']) / 1000
+
+def config_reader_get_focus_stepper_default_steps():
+    with open(microscope_cmd_conf, 'r') as m:
+        microscope_data = json.loads(m.read())
+    return int(microscope_data['work_steps_cnt_focus_stepper'])
+
+def config_reader_get_updown_stepper_default_steps():
+    with open(microscope_cmd_conf, 'r') as m:
+        microscope_data = json.loads(m.read())
+    return int(microscope_data['work_steps_cnt_updown_stepper'])
+
+def config_reader_get_leftright_stepper_default_steps():
+    with open(microscope_cmd_conf, 'r') as m:
+        microscope_data = json.loads(m.read())
+    return int(microscope_data['work_steps_cnt_leftright_stepper'])
+
+def config_reader_get_swap():
+    with open(microscope_cmd_conf, 'r') as m:
+        microscope_data = json.loads(m.read())
+    return microscope_data['swap_updown_and_lefnright_logic']
+
+def config_reader_focus_home_sign():
+    with open(microscope_cmd_conf, 'r') as m:
+        microscope_data = json.loads(m.read())
+    return microscope_data['home_btn_sign_focus_stepper']
+
+def config_reader_updown_home_sign():
+    with open(microscope_cmd_conf, 'r') as m:
+        microscope_data = json.loads(m.read())
+    return microscope_data['home_btn_sign_updown_stepper']
+
+def config_reader_leftright_home_sign():
+    with open(microscope_cmd_conf, 'r') as m:
+        microscope_data = json.loads(m.read())
+    return microscope_data['home_btn_sign_leftright_stepper']
--- web_viewer/modbus.py
+++ web_viewer/modbus.py
@@ -3,6 +3,8 @@ from pymodbus.exceptions import ModbusException
 
 from helpers import helper_get_my_ip
 from config_reader import config_reader_get_modbus_tcp_rtu_converter_port, config_reader_get_modbus_slave_id, config_reader_get_modbus_slave_timeout
+from config_reader import config_reader_get_focus_stepper_default_steps, config_reader_get_updown_stepper_default_steps, config_reader_get_leftright_stepper_default_steps, config_reader_get_swap
+from config_reader import config_reader_focus_home_sign, config_reader_updown_home_sign, config_reader_leftright_home_sign
 
 def modbus_connect_to_tcp_rtu_converter():
     ip = helper_get_my_ip()
@@ -19,53 +21,118 @@ def modbus_connect_to_tcp_rtu_converter():
 
 def modbus_get_battery_level():
     c.connect()
-    response = c.read_input_registers(17, 1, slave=slave_addr)
+
+    try:
+        response = c.read_holding_registers(17, 1, slave=slave_addr)
+    except ModbusException as exc:
+        print(f"ERROR: exception in pymodbus {exc}")
+
     c.close()
 
-    print(response.registers)
-    return response.registers
+    if response.isError():
+        ret = 0
+        print("[ERR] Read battery level failed")
+    else:
+        ret = response.registers
+
+    return ret
 
 def modbus_focus_motor_control(level):
     c.connect()
 
-    # if level == "upper":
-    #     c.write_register()
-    # elif level == "lower":
-    #     c.write_register()
+    if level == "upper":
+        c.write_register(12, 1, slave=slave_addr)
+    elif level == "lower":
+        c.write_register(12, -1, slave=slave_addr)
 
     c.close()
-    print()
 
 
 def modbus_light_control(level):
     c.connect()
 
-    # if level == "upper":
-    #     c.write_register()
-    # elif level == "lower":
-    #     c.write_register()
+    if level == "upper":
+        c.write_register(14, 1, slave=slave_addr)
+    elif level == "lower":
+        c.write_register(14, 0, slave=slave_addr)
 
     c.close()
-    print()
 
 
 def modbus_main_motors_control(position):
     c.connect()
 
-    # if position == "up":
-    #     c.write_register()
-    # elif position == "down":
-    #     c.write_register()
-    # elif position == "left":
-    #     c.write_register()
-    # elif position == "right":
-    #     c.write_register()
-    # elif position == "WORK":
-    #     c.write_register()
-    # elif position == "STOP":
-    #     c.write_register()
-    # elif position == "HOME":
-    #     c.write_register()
+    # Swap up and left AND right and down?
+    swap = config_reader_get_swap()
+
+    # 2  - up\down addr
+    # 4  - left\right
+    # 12 - focus
+
+    if position == "up":
+        if swap == "yes":
+            c.write_register(4, -1, slave=slave_addr)
+        else:
+            c.write_register(2, 1, slave=slave_addr)
+
+    elif position == "down":
+        if swap == "yes":
+            c.write_register(4, 1, slave=slave_addr)
+        else:
+            c.write_register(2, -1, slave=slave_addr)
+
+    elif position == "left":
+        if swap == "yes":
+            c.write_register(2, -1, slave=slave_addr)
+        else:
+            c.write_register(4, -1, slave=slave_addr)
+
+    elif position == "right":
+        if swap == "yes":
+            c.write_register(2, -1, slave=slave_addr)
+        else:
+            c.write_register(4, 1, slave=slave_addr)
+
+    elif position == "WORK":
+        updown_def_steps = int(config_reader_get_updown_stepper_default_steps())
+        leftright_def_steps = int(config_reader_get_leftright_stepper_default_steps())
+        focus_def_steps = int(config_reader_get_focus_stepper_default_steps())
+
+        if swap == "yes":
+            c.write_register(4, updown_def_steps, slave=slave_addr)
+        else:
+            c.write_register(2, updown_def_steps, slave=slave_addr)
+    
+        if swap == "yes":
+            c.write_register(2, leftright_def_steps, slave=slave_addr)
+        else:
+            c.write_register(4, leftright_def_steps, slave=slave_addr)
+
+        c.write_register(12, focus_def_steps, slave=slave_addr)
+
+    elif position == "STOP":
+        c.write_register(33, 1, slave=slave_addr)
+
+    elif position == "HOME":
+        focus_sign = config_reader_focus_home_sign()
+        updown_sign = config_reader_updown_home_sign()
+        leftright_sign = config_reader_leftright_home_sign()
+
+        if focus_sign == "+":
+            c.write_register(12, 32767, slave=slave_addr)
+        else:
+            c.write_register(12, -32767, slave=slave_addr)
+
+        if updown_sign == "+":
+            c.write_register(2, 32767, slave=slave_addr)
+        else:
+            c.write_register(2, -32767, slave=slave_addr)
+
+        if leftright_sign == "+":
+            c.write_register(4, 32767, slave=slave_addr)
+        else:
+            c.write_register(4, -32767, slave=slave_addr)
+    else:
+        print("[ERR] Wrong arg fr motors")
 
     c.close()
-    print()
