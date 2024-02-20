cb7cabd2d86dccb3638ff6e0864cd690b5789bfc
Aleksandr Anisimov <anisimov.alexander.s@gmail.com>
2024-02-20 19:36:27 +0300

    [server] Don't reconnect mannually. It turns out the modbus lib does it automatically

--- web_server/modbus.py
+++ web_server/modbus.py
@@ -12,6 +12,9 @@ from time import sleep
 from helpers import helper_get_my_ip
 import config_reader as conf_reader
 
+global last_bat_level
+last_bat_level = 0
+
 def __make_negative(value):
     negative = ~value
     negative += 1
@@ -38,47 +41,52 @@ def modbus_connect_to_tcp_rtu_converter(debug_mode):
     c = ModbusClient.ModbusTcpClient(
                     host=ip,
                     port=port,
-                    framer=ModbusSocketFramer,
-                    timeout=timeout)
+                    framer=ModbusSocketFramer)
+
+    c.connect()
+    assert c.connected
+    if c.connected == True:
+        print('Connected to Modbus TCP/RTU client OK')
+    else:
+        print('Connected to Modbus TCP/RTU client FAILED')
 
 
 def modbus_get_battery_level():
-    c.connect()
+    global c
+    global last_bat_level
 
     try:
         response = c.read_holding_registers(17, 1, slave=slave_addr)
     except ModbusException as exc:
         print(f"[ERR](Read battery level) --> Received ModbusException({exc}) from library")
         c.close()
-        return 0
+        return last_bat_level
 
     if response.isError():
         print(f"[ERR](Read battery level) --> Received Modbus library error({response})")
         c.close()
-        return 0
-    if isinstance(response, ExceptionResponse):
+        return last_bat_level
+    elif isinstance(response, ExceptionResponse): # THIS IS NOT A PYTHON EXCEPTION, but a valid modbus message
         print(f"[ERR](Read battery level) --> Received Modbus library exception ({response})")
-        # THIS IS NOT A PYTHON EXCEPTION, but a valid modbus message
         c.close()
+        return last_bat_level
+    else:
+        last_bat_level = response.registers
 
-    c.close()
-    return response.registers
+    return last_bat_level
 
 def modbus_focus_motor_control(level):
-    c.connect()
+    global c
 
     if level == "upper":
         c.write_register(12, 1, slave=slave_addr)
     elif level == "lower":
         c.write_register(12, 65535, slave=slave_addr)
 
-    c.close()
-
-
 def modbus_light_control(level):
-    c.connect()
-
+    global c
     global pwm_duty
+
     max_pwm_duty = conf_reader.get_led_pwm_max_power()
 
     if level == "upper":
@@ -94,12 +102,8 @@ def modbus_light_control(level):
             print("Led MIN power already ENABLED")
         c.write_register(14, pwm_duty, slave=slave_addr)
 
-    c.close()
-
-
 def modbus_main_motors_control(position):
-
-    c.connect()
+    global c
 
     # Swap up and left AND right and down?
     swap = conf_reader.get_swap()
@@ -202,4 +206,3 @@ def modbus_main_motors_control(position):
             print("[ERR] Unknown swap value in config file")
 
     sleep(0.02)
-    c.close()
