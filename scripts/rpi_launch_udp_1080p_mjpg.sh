ddfb914118c803a8ec903528c2cc7862ad4eb9d7
Aleksandr Anisimov <anisimov.alexander.s@gmail.com>
2024-04-04 15:48:33 +0300

    Revert "[server][stream] Add --eos-on-shutdown flag for gstreamer"
    This reverts commit 30689509c64bd061fb6241c8a9f4bb24a4b7f002.

--- scripts/rpi_launch_udp_1080p_mjpg.sh
+++ scripts/rpi_launch_udp_1080p_mjpg.sh
@@ -12,7 +12,7 @@ fi
 
 video=$(ls /sys/class/video4linux -1 | head -n1)
 sudo v4l2-ctl -d /dev/$video --set-fmt-video=width=1920,height=1080,pixelformat=MJPG -p 30 > /dev/null
-gst-launch-1.0 -v --eos-on-shutdown v4l2src device=/dev/$video io-mode=4 ! image/jpeg,width=1920,height=1080,type=video,framerate=30/1 ! rtpjpegpay ! queue ! udpsink host=$HOST port=5602 > /dev/null &
+gst-launch-1.0 -v v4l2src device=/dev/$video io-mode=4 ! image/jpeg,width=1920,height=1080,type=video,framerate=30/1 ! rtpjpegpay ! queue ! udpsink host=$HOST port=5602 > /dev/null &
 
 pid=`pgrep gst-launch-1.0`
 exit $pid
